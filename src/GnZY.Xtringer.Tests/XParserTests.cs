3db1598019f6907514d73f95c22d4f2e74b3ed53
Elcio Pereira <elcio.pereira@levelup.com.br>
2014-09-08 18:00:25 -0300

    Adding basic tests

---
+++ src/GnZY.Xtringer.Tests/XParserTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace GnZY.Xtringer.Tests
+{
+    [TestFixture]
+    public class XParserTests
+    {
+        [Test]
+        public void Encode_EmptyString()
+        {
+            new XParser().Parse("").Should().Be("");
+        }
+
+        [Test]
+        public void Encode_OneParameter()
+        {
+            dynamic parser = new XParser();
+
+            parser.name = "teste";
+
+            string s = parser.Parse("{name}");
+
+            s.Should().Be("teste");
+        }
+
+        [Test]
+        public void Encode_SameParameter()
+        {
+            dynamic parser = new XParser();
+
+            parser.name = "teste";
+
+            string s = parser.Parse("{name} {name}");
+
+            s.Should().Be("teste teste");
+        }
+
+        [Test]
+        public void Encode_TwoParameters()
+        {
+            dynamic parser = new XParser();
+
+            parser.name = "teste";
+            parser.last_name = "testado";
+
+            string s = parser.Parse("{name} {last_name}");
+
+            s.Should().Be("teste testado");
+        }
+
+        [Test]
+        public void Encode_FormatNumber()
+        {
+            dynamic parser = new XParser();
+
+            parser.color = 0xAABBCCDD;
+
+            string s = parser.Parse("testando {color:X8}");
+
+            s.Should().Be("testando AABBCCDD");
+        }
+
+        [Test]
+        public void Encode_FormatDateTime()
+        {
+            dynamic parser = new XParser();
+
+            parser.some_date = new DateTime(2014, 10, 6);
+
+            string s = parser.Parse("testando {some_date:yyyy/dd}");
+
+            s.Should().Be("testando 2014/06");
+        }
+
+        [Test]
+        public void Encode_FormatEscapeLeftBrace()
+        {
+            dynamic parser = new XParser();
+
+            parser.name = "teste";
+
+            string s = parser.Parse(@"testando {\{} {name}");
+            s.Should().Be("testando { teste");
+        }
+
+        [Test]
+        public void Encode_FormatEscapeRightBrace()
+        {
+            dynamic parser = new XParser();
+
+            parser.name = "teste";
+
+            string s = parser.Parse(@"testando {\} {name}");
+            s.Should().Be("testando } teste");
+        }
+
+        [Test]
+        public void Encode_FormatEscapeAll()
+        {
+            dynamic parser = new XParser();
+
+            parser.name = "teste";
+
+            string s = parser.Parse(@"testando {\{} {\} {name}");
+            s.Should().Be("testando { } teste");
+        }
+    }
+}
